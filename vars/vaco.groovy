05f6ec52946d1f987f3aed61b83cb5db0818fee7
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2024-11-29 14:37:40 +0530

    Create vaco.groovy

---
+++ vars/vaco.groovy
@@ -0,0 +1,1 @@
+ansiblePlaybook credentialsId: 'f4fcd509-ca78-47b4-9610-c54171fda58b', installation: 'Ansible', inventory: '/resources/aws_ec2.yaml', playbook: '/resources/vault.yaml', vaultTmpPath: ''
