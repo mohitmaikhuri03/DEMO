f17db376065a70a5232bf2ebc26d0da7a900541d
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2024-11-29 15:12:33 +0530

    Update installvault.groovy

--- vars/installvault.groovy
+++ vars/installvault.groovy
@@ -3,8 +3,8 @@ def call() {
                         credentialsId: 'f4fcd509-ca78-47b4-9610-c54171fda58b', 
                         disableHostKeyChecking: true,
                         installation: 'Ansible', 
-                        inventory: 'workspace/resources/aws_ec2.yaml',
-                        playbook: 'workspace/resources/vault.yaml', 
+                        inventory: 'workspace/A6/resources/aws_ec2.yaml',
+                        playbook: 'workspace/A6/resources/vault.yaml', 
                         vaultTmpPath: ''
                     )
 
